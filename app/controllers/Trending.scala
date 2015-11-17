b61af9d8d019e7ffa742817820960cab5ca57ad4
mclutha <mahanaclutha@gmail.com>
2015-11-17 16:52:27 +0000

    a

--- app/controllers/Trending.scala
+++ app/controllers/Trending.scala
@@ -6,6 +6,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 case class TermResult(term: String, inTopBox: Boolean)
+
 case class CountryResult(country: String, termResults: List[TermResult])
 
 class Trending extends Controller {
@@ -13,23 +14,23 @@ class Trending extends Controller {
   val google = new GoogleResultService
 
 
-
   def index(countryString: String) = Action.async {
     val trending = TrendingSearchTerms.get().map(_.toList)
-      .flatMap{list =>
-      Future.sequence(list.flatMap{case (country, terms) =>
-        if(country.equals(countryString)){
-          Some(Future.sequence(terms.map { term =>
-            google.getImage(s"https://www.google.com?q=$term")
-              .map(result => result.report.nonEmpty).map(bool => TermResult(term, bool))
-          }).map(list => CountryResult(country, list)))
-    }else{
-          None
-        }
+      .flatMap { list =>
+        Future.sequence(list.flatMap { case (country, terms) =>
+          if (country.equals(countryString)) {
+            Some(Future.sequence(terms.map { term =>
+              google.getImage(s"https://www.google.com?q=$term")
+                .map(result => result.report.nonEmpty).map(bool => TermResult(term, bool))
+            }).map(list => CountryResult(country, list)))
+          } else {
+            None
+          }
 
-      })}
+        })
+      }.map{_.head}
 
-    trending.map(Ok(_))
+    Future(Ok("test"))
   }
 
 }
